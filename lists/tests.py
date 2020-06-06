08ac834e33c9e30533c41a01ac23399c7d3c7c69
luoe <luo.e@yahoo.com>
2020-06-06 10:14:14 +0800

    new URL, view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -6,6 +6,17 @@ from lists.views import home_page
 from django.template.loader import render_to_string
 from lists.models import Item
 
+class ListViewTest(TestCase):
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+        
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        
+        self.assertContains(response,'itemey 1')
+        self.assertContains(response,'itemey 2')
+
 class ItemModelTest(TestCase):
     
     def test_saving_and_retribeing_items(self):
@@ -24,10 +35,7 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text,'The first (ever) list item')
         self.assertEqual(second_saved_item.text,'Item the second')
-
-
-class HomePageTest(TestCase):
-
+        
     def test_displays_all_list_items(self):
         Item.objects.create(text='itemey 1')
         Item.objects.create(text='itemey 2')
@@ -36,6 +44,10 @@ class HomePageTest(TestCase):
         
         self.assertIn('itemey 1',response.content.decode())
         self.assertIn('itemey 2',response.content.decode())
+
+
+class HomePageTest(TestCase):
+
 
     def test_uses_home_template(self):
         response = self.client.get('/')
@@ -53,7 +65,7 @@ class HomePageTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/',data={'item_text':'A new list item'})
         self.assertEqual(response.status_code,302)
-        self.assertEqual(response['location'],'/')
+        self.assertEqual(response['location'],'/lists/the-only-list-in-the-world/')
         
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
