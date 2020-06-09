e2ea4fec4fd5e375610ec0259704a90c6d037bb1
luoe <luo.e@yahoo.com>
2020-06-09 16:50:05 +0800

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -8,6 +8,25 @@ MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text,[row_text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time>MAX_WAIT:
+                    raise e
+                time.sleep(0.5)  
+                
+    def tearDown(self):
+        self.browser.quit()   
+        
     def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app.She goes
         
@@ -60,74 +79,28 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
         
         # Satisfied, they both go back to sleep
-        
 
-    def wait_for_row_in_list_table(self, row_text):
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text,[row_text for row in rows])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time>MAX_WAIT:
-                    raise e
-                time.sleep(0.5)    
-
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-        
-    def tearDown(self):
-        self.browser.quit()
-        
-    def test_can_start_a_list_and_retrieve_it_later(self):
-        #Edith has heard about a cool new online to-do sapp.She goes
-        #to check out its homepage
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
         self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
 
-        #She notices the page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)
-        
-
-        #She is invited to enter a to-do item straight away
+        # She notices the input box ins nicely centered
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-         )
-        
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+            )
 
-        #She types "Buy peacock feathers" into a text box (Edith's hobby 
-        #is tying fly-fishing lures
-        inputbox.send_keys('Buy peacock feathers')
-        
-
-        #when she hits enter, the page updates , and now the page lists 
-        # "1:Buy peacock feathers" as an item in a to-do list
+        # She starts a new list and sees the input is nicely
+        # centered there too
+        inputbox.send_keys('testing')
         inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
-
-        #There is still a text box inviting her to add another item.She
-        #enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        self.wait_for_row_in_list_table('1: testing')
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Use peacock feathers to make a fly')
-        inputbox.send_keys(Keys.ENTER)
-        
-        
-        #The page updates again, and now shows both items on her list
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
-        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
-
-        #Edith wonders whether the site will remember her list. Then she sees 
-        #that the site has generated a unique URL for her -- there is some
-        #explanatory text to that effect.
-        self.fail('Finish the test!')
-
-        #She visits that URL  - her to-do list is still there
-
-        #Satisfied, she goes back to sleep
-        
-
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+            )
