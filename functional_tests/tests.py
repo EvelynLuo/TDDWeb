5b8b65cc06c740a90b5cf3e3dfb97af942f68662
Evelyn <luo.e@yahoo.com>
2020-06-25 19:19:25 +0800

    add requirements.txt for virlenv

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,20 +1,23 @@
+from selenium import webdriver      # (1)
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
+from selenium import webdriver
 import time
+import unittest
+import os
 
 MAX_WAIT = 10
 
-
 class NewVisitorTest(StaticLiveServerTestCase):
-
     def setUp(self):
         self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
 
     def tearDown(self):
         self.browser.quit()
-
 
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
@@ -29,49 +32,48 @@ class NewVisitorTest(StaticLiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-
     def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.live_server_url)    # (3)
 
-        # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
-        # She types "Buy peacock feathers" into a text box (Edith's hobby
-        # is tying fly-fishing lures)
+        # She types "Buy peacock feathers" inti a text box (Edith's hobby
+        # is typing fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
 
-        # When she hits enter, the page updates, and now the page lists
-        # "1: Buy peacock feathers" as an item in a to-do list table
+        # when she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-        # There is still a text box inviting her to add another item. She
-        # enters "Use peacock feathers to make a fly" (Edith is very
-        # methodical)
+        # There is still a text box inviting her to add nother item. She 
+        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
-        # The page updates again, and now shows both items on her list
-        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        # The page updates again, and now shows both items on her lists
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+        # Edith wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanation text to that effect.
+
+        # She visits that URL - her to-do list is still there.
 
         # Satisfied, she goes back to sleep
-
-
+    
     def test_multiple_users_can_start_lists_at_different_urls(self):
-        # Edith start a new todo list
+        # Edith starts a new to-do list
         self.browser.get(self.live_server_url)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy peacock feathers')
@@ -83,13 +85,12 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertRegex(edith_list_url, '/lists/.+')
 
         # Now a new user, Francis, comes along to the site.
-
-        ## We use a new browser session to make sure that no information
+        ## we use a new browser session to make sure that no information
         ## of Edith's is coming through from cookies etc
         self.browser.quit()
         self.browser = webdriver.Firefox()
 
-        # Francis visits the home page.  There is no sign of Edith's
+        # Francis visits the home page. There is no sign of Edith's
         # list
         self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
@@ -97,7 +98,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertNotIn('make a fly', page_text)
 
         # Francis starts a new list by entering a new item. He
-        # is less interesting than Edith...
+        # is less interesting than Edith ....
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
@@ -111,23 +112,24 @@ class NewVisitorTest(StaticLiveServerTestCase):
         # Again, there is no trace of Edith's list
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
-        self.assertIn('Buy milk', page_text)
+        self.assertIn('Buy milk', page_text, page_text)
 
         # Satisfied, they both go back to sleep
+
     def test_layout_and_styling(self):
-        # Edith goes to the home page  
+        # Edith goes to the home page
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 768)
-        
-        # She notices the input box is nicely centered
+
+        # She notices the input box ins nicely centered
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
             delta=10
-            )
-        
-        # She starts a new list and seese the input is nicely 
+        )
+
+        # She starts a new list and sees the input is nicely
         # centered there too
         inputbox.send_keys('testing')
         inputbox.send_keys(Keys.ENTER)
@@ -137,4 +139,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
             delta=10
-            )
+        )
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
