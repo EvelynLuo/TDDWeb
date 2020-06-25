1feb80045e777f3930b5cd0125b184ea655cd574
Evelyn <luo.e@yahoo.com>
2020-06-25 13:08:44 +0800

    refactor templates to use a base template  FT:AssertionError

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -119,6 +119,14 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 768)
         
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+            )
+        
         # She starts a new list and seese the input is nicely 
         # centered there too
         inputbox.send_keys('testing')
